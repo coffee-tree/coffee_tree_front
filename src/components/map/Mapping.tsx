5a75fa69a5c9e2c1c3f81af18925e7a1357d39ed
KYUTAE PARK <rbxo0109@gmail.com>
2023-11-16 21:24:48 +0900

    feat: marker이동

--- src/components/map/Mapping.tsx
+++ src/components/map/Mapping.tsx
@@ -1,5 +1,8 @@
 import React ,{useEffect} from "react";
 import MarkerArr from "./Marker";
+import {MapInterpolation, Mapinclination} from "./Maputil/MapUtil";
+
+import test from "../../test.json"
 import "./Mapping.css"
 
 declare global {
@@ -7,6 +10,7 @@ declare global {
       kakao: any;
     }
   }
+const {coordinate} = test;
 const Mapping = () => {
     useEffect(()=>{
         const mapContainer = document.querySelector(".map");
@@ -15,11 +19,26 @@ const Mapping = () => {
             level: 3,
         };
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
+        
+        const mapProjection = map.getProjection();
+        let prev = mapProjection.pointFromCoords(new window.kakao.maps.LatLng(coordinate[4]["x"],coordinate[4]["y"]));
+        let next = mapProjection.pointFromCoords(new window.kakao.maps.LatLng(33.450701, 126.570667));
+        const inclination = Mapinclination(prev,next);
         MarkerArr.forEach(ele=>{
             ele.setMap(map);
         })
+        const markerMove = () =>{
+            let new_prev = MapInterpolation(prev,next,inclination);//위치 좌표 받기
+            let point = new window.kakao.maps.Point(new_prev.x,new_prev.y);//지도 좌표 변환 준비하기
+            let new_coor = mapProjection.coordsFromPoint(point);//지도 좌표로 변환하기
+            let screen = new window.kakao.maps.LatLng(new_coor.Ma,new_coor.La);//마커와 setPosition하기 위한
+            prev = new_prev;
+            MarkerArr[4].setPosition(screen);
+            if(new_prev.x!==next.x)
+                window.requestAnimationFrame(markerMove);
+        }
+        window.requestAnimationFrame(markerMove);
     },[])
-
     return (
         <div className="map">
 
--- src/components/map/Marker.tsx
+++ src/components/map/Marker.tsx
@@ -9,11 +9,18 @@ declare global {
 
 const {coordinate} = test;//marker가 원래 있어야 하는 장소
 let MarkerArr = new Array(coordinate.length).fill(0).map((v,i)=>{
+    const imageSrc = "./image/MapMarkerSvg.svg";
+    const imageSize = new window.kakao.maps.Size(16,16);
+    const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize)
     const {x,y}:{x:number,y:number} = coordinate[i];
     const markerPosition = new window.kakao.maps.LatLng(x,y);
-    return new window.kakao.maps.Marker({
-        position:markerPosition
+    
+    const marker = new window.kakao.maps.Marker({
+        position:markerPosition,
+        image:markerImage
     });
+    return marker
 })
+
 
 export default MarkerArr
